8f6fd1545b6be6d53435c5f9fac3558f1b7f2f39
mcheng3 <michael.cheng65@gmail.com>
2017-02-10 12:32:51 -0500

    Update QueenBoard.java

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -10,8 +10,20 @@ public class QueenBoard{
 	return solveHelper(board.length); 
     }
 
-    public boolean solveHelper(int queensRemaining){
-	if(queensRemaining == 0) return true;
-	else if(
+    private boolean solveHelper(int row){
+	if(row == 0){
+	    return true;
+	}
+	else if(row > 0 && row <= board.length){
+	    for(int col = board.length, col > 0, col++){
+		if(board[row][col] == 0){
+		    board[row][col] = 1;
+		}
+	    }
+	}
+		    
+		
     }
+    private void fill(int row, int col){
+	if(
 
