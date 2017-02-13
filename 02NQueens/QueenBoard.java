5824187f8d4ae05dd369c626eb505b0c5acb24d2
mcheng3 <michael.cheng65@gmail.com>
2017-02-13 12:33:04 -0500

    Update QueenBoard.java

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -15,15 +15,27 @@ public class QueenBoard{
 	    return true;
 	}
 	else if(row > 0 && row <= board.length){
-	    for(int col = board.length, col > 0, col++){
-		if(board[row][col] == 0){
-		    board[row][col] = 1;
-		}
-	    }
+	    for(int col = 0; col = board.length; col++){
+		fill(row, col);
 	}
 		    
 		
     }
     private void fill(int row, int col){
-	if(
+	int i = 1;
+	for(int c = col + 1; c < board.length; c++){
+
+	    if(row - i >= 0 || col - i >= 0){
+		board[row - 1][c] = 1;
+	    }
+	    if(row + i < board.length || col - i >= 0){
+		board[row + 1][c] = 1;
+	    }
+	    board[row][c] = 1;
+	    i ++;
+	}
+
+
+		
+	    
 
