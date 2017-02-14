53c7c4aef5045be5094c0948eecdc8274cf00b90
Michael Cheng <Michael Cheng>
2017-02-13 23:18:01 -0500

    Add counting functionality to Queenboard.java

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,14 +1,17 @@
 public class QueenBoard{
 	private int[][] board;
 	private int solutionCount;
+	private boolean hasCounted = false;
 	
 	public QueenBoard(int n){
 		board = new int[n][n];
 	}
 
 	public boolean solve(){
-		return solveHelper(0); 
-
+		if(solveHelper(0)){
+			return true;
+		} 
+		else return false;
 	}
 
 	private boolean solveHelper(int row){
@@ -30,6 +33,37 @@ public class QueenBoard{
 		}
 	}
 
+	public void countSolutions(){
+		board = new int[board.length][board.length]; //reset board
+		solutionCount = countHelper(0);
+		hasCounted = true;
+	}
+
+
+	private int countHelper(int row){
+		int sum = 0;
+		if(row == board.length){
+			return 1;
+		}
+		else{	
+			for(int c = 0; c < board.length; c ++){
+					if(board[row][c] == 0){
+						addQueen(row, c);
+						if(true){ //I have no idea why this works. I just randomly put it there for testing purposes and it actuallyworked.
+							sum += countHelper(row + 1);
+						}
+						removeQueen(row, c);
+					}	
+
+			}
+			return sum;
+		}
+	}
+
+	public int getSolutionCount(){
+		if(!hasCounted) return -1;
+		else return solutionCount;
+	}
 
 	private void addQueen(int row, int col){
 		int i = 1;
@@ -44,7 +78,7 @@ public class QueenBoard{
 			board[r][col] += 2;
 			i ++;
 		}
-		System.out.println("Add\n" + toStringDebug());
+		//System.out.println("Add\n" + toStringDebug());
 	}
 
 	private void removeQueen(int row, int col){
@@ -60,7 +94,7 @@ public class QueenBoard{
 			board[r][col] -= 2;
 			i ++;
 		}
-		System.out.println("Remove\n" + toStringDebug());
+		//System.out.println("Remove\n" + toStringDebug());
 	}
 
 	private String toStringDebug(){
@@ -87,9 +121,49 @@ public class QueenBoard{
 	}
 
 	public static void main(String[] args){
-		QueenBoard q = new QueenBoard(4);
-		System.out.println(q.solve());
-		System.out.println(q.toString());
+		QueenBoard q1 = new QueenBoard(1);
+		System.out.println(q1.solve());
+		System.out.println(q1.toString());
+		System.out.println("Count: " + q1.getSolutionCount());
+		q1.countSolutions();
+		System.out.println("Count: " + q1.getSolutionCount());
+		System.out.println(q1.toString());
+		QueenBoard q2 = new QueenBoard(2);
+		System.out.println(q2.solve());
+		System.out.println(q2.toString());
+		System.out.println("Count: " + q2.getSolutionCount());
+		q2.countSolutions();
+		System.out.println("Count: " + q2.getSolutionCount());
+		System.out.println(q2.toString());
+		QueenBoard q3 = new QueenBoard(3);
+		System.out.println(q3.solve());
+		System.out.println(q3.toString());
+		System.out.println("Count: " + q3.getSolutionCount());
+		q3.countSolutions();
+		System.out.println("Count: " + q3.getSolutionCount());
+		System.out.println(q3.toString());
+		QueenBoard q4 = new QueenBoard(4);
+		System.out.println(q4.solve());
+		System.out.println(q4.toString());
+		System.out.println("Count: " + q4.getSolutionCount());
+		q4.countSolutions();
+		System.out.println("Count: " + q4.getSolutionCount());
+		System.out.println(q4.toString());
+		QueenBoard q5 = new QueenBoard(9);
+		System.out.println(q5.solve());
+		System.out.println(q5.toString());
+		System.out.println("Count: " + q5.getSolutionCount());
+		q5.countSolutions();
+		System.out.println("Count: " + q5.getSolutionCount());
+		System.out.println(q5.toString());
+		/*QueenBoard q9 = new QueenBoard(9);
+		System.out.println(q9.solve());
+		System.out.println(q9.toString());
+		System.out.println("Count: " + q9.getSolutionCount());
+		q9.countSolutions();
+		System.out.println("Count: " + q9.getSolutionCount());
+		System.out.println(q9.toString());
 
+		*/
 	}
 }
