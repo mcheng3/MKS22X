7693021cf97657091ec95b78be8bd2600bea2a55
Michael Cheng <Michael Cheng>
2017-02-13 21:56:32 -0500

    Fix solve() in QueenBoard.java

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,41 +1,83 @@
-public class QueenBoard{
-    private int[][]board;
-    private int solutionCount;
-    
-    public QueenBoard(int n){
-	board = int[n][n];
-    }
-
-    public boolean solve(){
-	return solveHelper(board.length); 
-    }
-
-    private boolean solveHelper(int row){
-	if(row == 0){
-	    return true;
+public class Row{
+	private int[][] board;
+	private int solutionCount;
+	
+	public Row(int n){
+		board = new int[n][n];
 	}
-	else if(row > 0 && row <= board.length){
-	    for(int col = 0; col = board.length; col++){
-		fill(row, col);
-	}
-		    
-		
-    }
-    private void fill(int row, int col){
-	int i = 1;
-	for(int c = col + 1; c < board.length; c++){
 
-	    if(row - i >= 0 || col - i >= 0){
-		board[row - 1][c] = 1;
-	    }
-	    if(row + i < board.length || col - i >= 0){
-		board[row + 1][c] = 1;
-	    }
-	    board[row][c] = 1;
-	    i ++;
+	public boolean solve(){
+		return solveHelper(0); 
+
+	}
+
+	private boolean solveHelper(int row){
+		if(row == board.length){
+			return true;
+		}
+		else{	
+			for(int c = 0; c < board.length; c ++){
+					if(board[row][c] == 0){
+						addQueen(row, c);
+						if(solveHelper(row + 1)){
+							return true;
+						}
+						removeQueen(row, c);
+					}	
+
+			}
+			return false;
+		}
 	}
 
 
-		
-	    
+	private void addQueen(int row, int col){
+		int i = 1;
+		board[row][col] = 1;
+		for(int r = row + 1; r < board.length; r++){
+			if(col - i >= 0){
+				board[r][col - i] += 2;
+			}
+			if(col + i < board.length){
+				board[r][col + i] += 2;
+			}
+			board[r][col] += 2;
+			i ++;
+		}
+		System.out.println("Add\n" + toString());
+	}
 
+	private void removeQueen(int row, int col){
+		int i = 1;
+		board[row][col] = 0;
+		for(int r = row + 1; r < board.length; r++){
+			if(col - i >= 0){
+				board[r][col - i] -= 2;
+			}
+			if(col + i < board.length){
+				board[r][col + i] -= 2;
+			}
+			board[r][col] -= 2;
+			i ++;
+		}
+		System.out.println("Remove\n" + toString());
+	}
+
+	public String toString(){
+		String ary = "";
+		for(int r = 0; r < board.length; r++){
+			for(int c = 0; c < board.length; c ++){
+				ary += board[r][c] + " ";
+			}
+			ary += "\n";
+		}
+		return ary;
+	}
+
+	public static void main(String[] args){
+		Row q = new Row(4);
+		System.out.println(q.solve());
+		System.out.println(q.toString());
+
+	}
+}
