585c5608a889a709d8a9a130488abf0f75cd86b0
mcheng3 <michael.cheng65@gmail.com>
2017-02-16 12:32:37 -0500

    Update KnightBoard.java

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -13,6 +13,14 @@ public class KnightBoard{
 	*/
     }
 
+    private void clear(){
+	for(int r = 0; r<board.length; r++){
+	    for(int c = 0; c<board[0].length; c++){
+		board[r][c] = 0;
+	    }
+	}
+    }
+
     public String toString(){
 	String ary = "";
 	int number;
@@ -41,7 +49,16 @@ public class KnightBoard{
 	 
 
     public void solve(){
-	if(solveH(0, 0, 1)) hasSolved = true;
+	boolean dontStop = true;
+	for(int r = 0; r < board.length || dontStop; r++){
+	    for(int c = 0; c<board[0].length || dontStop; c++){
+		if(solveH(r, c, 1)){
+		    hasSolved = true;
+		    dontStop = false;
+		}
+		else clear();
+	    }
+	}     
     }
 
     public boolean solveH(int row, int col, int level){
@@ -49,10 +66,14 @@ public class KnightBoard{
 	    return true;
 	}
 	else{
-	    if(board[row][col] == 0){
-		
-		if(solveH(
+	    try{
+		if(board[row][col] == 0){
+		board[row][col] = level;
+		return 
+	    }
+	    return false;
     }
+
 
     public static void main(String[] args){
 	KnightBoard k = new KnightBoard(4, 5);
