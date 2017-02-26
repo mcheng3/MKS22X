1beef467d960913513212e5d207f42f3d09df395
Michael Cheng <Michael Cheng>
2017-02-26 04:06:49 -0500

    Super slow working version

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -1,93 +1,96 @@
 public class KnightBoard{
-    private int[][] board;
-    private boolean hasSolved = true;
+	private int[][] board;
+	private boolean hasSolved = true;
 
-    public KnightBoard(int startingRows, int startingCols){
-	board = new int[startingRows][startingCols];
+	public KnightBoard(int startingRows, int startingCols){
+		board = new int[startingRows][startingCols];
 	/*
 	  for(int r = 0; r<board.length; r++){
-	    for(int c = 0; c<board[0].length; c++){
+		for(int c = 0; c<board[0].length; c++){
 		board[r][c] = 10;
-	    }
+		}
 	}
 	*/
-    }
+}
 
-    private void clear(){
+private void clear(){
 	for(int r = 0; r<board.length; r++){
-	    for(int c = 0; c<board[0].length; c++){
-		board[r][c] = 0;
-	    }
+		for(int c = 0; c<board[0].length; c++){
+			board[r][c] = 0;
+		}
 	}
-    }
+}
 
-    public String toString(){
+public String toString(){
 	String ary = "";
 	int number;
 	if(!hasSolved) return ary;
 	for(int r = 0; r < board.length; r++){
-	    number = board[r][0];
+		number = board[r][0];
 		if(number < 10){
-		    ary += " " + number;
+			ary += " " + number;
 		}
 		else{
-		    ary += number;
+			ary += number;
 		} 
-	    for(int c = 1; c < board[0].length; c++){
-		number = board[r][c];
-		if(number < 10){
-		    ary += "  " + number;
+		for(int c = 1; c < board[0].length; c++){
+			number = board[r][c];
+			if(number < 10){
+				ary += "  " + number;
+			}
+			else{
+				ary += " " +  number;
+			}
 		}
-		else{
-		    ary += " " +  number;
-		}
-	    }
-	    ary += "\n";
+		ary += "\n";
 	}
 	return ary;
-    }
-	 
+}
 
-    public void solve(){
+
+public void solve(){
 	boolean dontStop = true;
 	solveH(0, 0, 1);    
-    }
+}
 
-    public boolean solveH(int row, int col, int level){
-        if(level == board.length * board[0].length){
-	    board[row][col] = level;
-	    return true;
+public boolean solveH(int row, int col, int level){
+	System.out.println(row + " " + col + " " + level);
+	System.out.println(toString());
+	if(level == board.length * board[0].length ){
+		board[row][col] = level;
+		return true;
 	}
 	else{
-	    try{
-		if(board[row][col] == 0){
-		board[row][col] = level;
-		if(solveH(row + 2, col + 1, level + 1)||
-		    solveH(row + 2, col - 1, level + 1)||
-		    solveH(row - 2, col + 1, level + 1)||
-		    solveH(row - 2, col - 1, level + 1)||
-		    solveH(row + 1, col + 2, level + 1)||
-		    solveH(row + 1, col - 2, level + 1)||
-		    solveH(row - 1, col + 2, level + 1)||
-		   solveH(row - 1, col - 2, level + 1)){
-		    return true;
+		try{
+			if(board[row][col] == 0){
+				board[row][col] = level;
+				if(solveH(row + 1, col + 2, level + 1)||
+					solveH(row + 1, col - 2, level + 1)||
+					solveH(row - 2, col + 1, level + 1)||
+					solveH(row - 2, col - 1, level + 1)||
+					solveH(row + 2, col + 1, level + 1)||
+					solveH(row + 2, col - 1, level + 1)||
+					solveH(row - 1, col + 2, level + 1)||
+					solveH(row - 1, col - 2, level + 1)){
+					return true;
+			}
+			board[row][col] = 0;
+
+
 		}
-		board[row][col] = 0;
+
 		}
-		System.out.println(toString());
-	    }
-	    catch(IndexOutOfBoundsException e){
-		return false;
-	    }
-		
-	    return false;
+		catch(IndexOutOfBoundsException e){
+			return false;
+		}
 	}
-    }
+	return false;
+}
 
 
-    public static void main(String[] args){
-	KnightBoard k = new KnightBoard(4, 5);
+public static void main(String[] args){
+	KnightBoard k = new KnightBoard(8, 8);
 	k.solve();
 	System.out.println(k.toString());
-    }
+}
 }
