d9a5dbeea829d6221a78d2e2a53b417df75d3d15
mcheng3 <michael.cheng65@gmail.com>
2017-03-10 12:31:29 -0500

    Copy from Naotaka's repo (partner)

---
+++ USACO/USACO.java
@@ -0,0 +1,77 @@
+import java.util.*;
+import java.io.*;
+//Worked with Naotaka Kinoshita
+public class USACO {
+    
+    private int[][]pasture;
+    private int[][] instructions;
+    public USACO() {
+    }
+
+    public int bronze(String filename) {
+	int rows = 0;
+	int cols = 0;
+	int instruct = 0;
+	int elevation = 0;
+	int[] firstLine = new int[4];
+	try {
+	    Scanner scan = new Scanner(new File(filename));
+	    for (int i = 0; i < 4; i ++) {
+		firstLine[i] = scan.nextInt();
+	    }
+	    rows = firstLine[0];
+	    cols = firstLine[1];
+	    elevation = firstLine[2];
+	    instruct = firstLine[3];
+	    pasture = new int[rows][cols];
+	    for (int i = 0; i < rows; i ++) {
+		for (int c = 0; c < cols; c ++) {
+		    pasture[i][c] = scan.nextInt();
+		}
+	    }
+	    instructions = new int[instruct][3];
+	    for (int i = 0; i < instruct; i ++) {
+		for (int c = 0; c < 3; c ++) {
+		    instructions[i][c] = scan.nextInt();
+		}
+	    }
+	}catch (FileNotFoundException e) {
+	    System.out.println("File Not Found");
+	    System.exit(0);
+	}
+	for (int i = 0; i < instructions.length; i ++) {
+	    stomp(instructions[i][0], instructions[i][1], instructions[i][2]);
+	}
+  return 0
+    }
+
+    public boolean stomp(int r, int c, int d) {
+        int highR = r - 1;
+	int highC = c - 1;
+	for (int row = r - 1; row < r+2; row ++) {
+	    for (int col = c - 1; col < c+2; col ++) {
+		if (pasture[row][col] > pasture[highR][highC]) {
+		    highR = row;
+		    highC = col;
+		}
+	    }
+	}
+	int highest = pasture[highR][highC];
+	pasture[highR][highC] -= d;
+	for (int row = r - 1; row < r+2; row ++) {
+	    for (int col = c - 1; col < c+2; col ++) {
+		if (pasture[row][col] + d >= pasture[highR][highC]) {
+		    pasture[row][col] = pasture[highR][highC];
+		}
+	    }
+	}
+	return true;
+    }
+    
+    
+    
+    public static void main(String[]args) {
+	USACO x = new USACO();
+	System.out.println(x.bronze("makelake.1.in"));
+    }
+}
