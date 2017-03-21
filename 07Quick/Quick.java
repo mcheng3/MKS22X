b6a2569835cdd94356e4026dd534f42ec596a01e
mcheng3 <michael.cheng65@gmail.com>
2017-03-21 12:31:45 -0400

    Update part()

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -5,21 +5,45 @@ public class Quick{
 	int random = (int)(Math.random() * (end - start -1 ) + start + 1);
 	System.out.println(random);
 	int pivot = data[random];
-	while(start <= end){
+	System.out.println(pivot);
+	data = swap(data, start, random);
+	int i = start + 1;
+        int lt = start;
+	int gt = end;
+	while(i <= gt){
+	    /*
 	    while(data[start] < pivot) start++;
-	    while(data[end] > pivot) end++;
+	    while(data[end] > pivot) end--;
 	    if( start <= end){
-		int hold = data[start++];
-		data[start++] = data[end--];
-		data[end--] = hold;
+		data = swap(data, start, end);
+		start++;
+		end--;
+	    }*/
+	    if(data[i] == pivot) i++;
+	    else if(data[i] < pivot){
+		data = swap(data, i, lt);
+		lt++;
+		i++;
+	    }
+	    else{
+		data = swap(data, i, gt);
+		gt--;
 	    }
 	}
 	System.out.println(Arrays.toString(data));
 	return pivot;
 	
     }
+
+    private static int[] swap(int[] a, int i, int j){
+	int hold = a[i];
+	a[i] = a[j];
+	a[j] = hold;
+	return a;
+    }
+
     public static void main(String[] args){
-	int[] data = {5, 4, 3, 2, 1, 0};
-	System.out.println(part(data, 0, 3));
+	int[] data = {5, 4, 3, 2, 1, 0, 55, 999, 999, 999, 999, 999, 999, 999, 55, 55, 55};
+	System.out.println(part(data, 0, data.length - 1));
     }
 }
