fc6aeff04fcc3b94997964133bd0b7d4c6af5fa4
mcheng3 <michael.cheng65@gmail.com>
2017-03-23 12:19:04 -0400

    Complete quicksort()

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,24 +1,18 @@
 import java.util.*;
 
 public class Quick{
-    public static int part(int[] data, int start, int end){
+    public static void part(int[] data, int start, int end){
+	if(end - start <= 1) return;
+	else{
 	int random = (int)(Math.random() * (end - start -1 ) + start + 1);
-	System.out.println(random);
+	//System.out.println(random);
 	int pivot = data[random];
-	System.out.println(pivot);
+	//System.out.println(pivot);
 	data = swap(data, start, random);
 	int i = start + 1;
         int lt = start;
 	int gt = end;
 	while(i <= gt){
-	    /*
-	    while(data[start] < pivot) start++;
-	    while(data[end] > pivot) end--;
-	    if( start <= end){
-		data = swap(data, start, end);
-		start++;
-		end--;
-	    }*/
 	    if(data[i] == pivot) i++;
 	    else if(data[i] < pivot){
 		data = swap(data, i, lt);
@@ -30,10 +24,19 @@ public class Quick{
 		gt--;
 	    }
 	}
-	System.out.println(Arrays.toString(data));
-	return pivot;
+	//System.out.println(Arrays.toString(data));
+	    part(data, start, lt);
+	    part(data, gt, end);
+	}
 	
     }
+
+    public static void quicksort(int[] a){
+	part(a, 0, a.length - 1);
+    }
+
+    //public static int quickselect(int[a], int k){
+	
 
     private static int[] swap(int[] a, int i, int j){
 	int hold = a[i];
@@ -43,7 +46,8 @@ public class Quick{
     }
 
     public static void main(String[] args){
-	int[] data = {5, 4, 3, 2, 1, 0, 55, 999, 999, 999, 999, 999, 999, 999, 55, 55, 55};
-	System.out.println(part(data, 0, data.length - 1));
+	int[] data = {999,999, 999, 4, 3, 2, 1, 0, 55, 999, 999, 999, 999, 999, 999, 999, 55, 55, 55, 654 ,6546,54654,6612 ,135,546,4354,384,384};
+	quicksort(data);
+	System.out.println(Arrays.toString(data));
     }
 }
