1eae2dcef024f6b4073c166b052bccae7300c5ee
mcheng3 <michael.cheng65@gmail.com>
2017-04-21 06:54:19 -0400

    Update Postfix.java

--- Postfix.java
+++ Postfix.java
@@ -1,2 +1,17 @@
 public class Postfix{
 	
+	public int eval(String eq){
+		String[] ary = eq.split(" ");
+		Stack s = new Stack();
+		for(int i = 0; i < ary.length; i++){
+			String c = ary[i];
+			if(isOperation(c)){
+				s.push(applyOperation(operation, s.pop(), s.pop()));
+			}
+			else{
+				s.push(c);
+			}
+		}
+	}
+
+}
