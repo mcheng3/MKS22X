f1cba3872ac4c2cdd0ac49bf4826b7f63bdd933b
Michael Cheng <Michael Cheng>
2017-04-24 20:21:29 -0400

    Finish StackCalc.java

---
+++ 11StackCalc/StackCalc.java
@@ -0,0 +1,54 @@
+import java.util.Stack;
+public class StackCalc{
+	
+	public static int eval(String eq){
+		String[] ary = eq.split(" ");
+		Stack<Double> s = new Stack<Double>();
+		for(int i = 0; i < ary.length; i++){
+			String c = ary[i];
+			if(isOperation(c)){
+				s.push(applyOperation(c, s.pop(), s.pop()));
+			}
+			else{
+				s.push(Double.parseDouble(c));
+			}
+		}
+		double ans = s.pop();
+		return (int) ans;
+	}
+
+	private static boolean isOperation(String character){
+		if(character.charAt(0) >= '0' && character.charAt(0) <='9'){
+			//System.out.println(character);
+			return false;
+		}
+		else return true;
+	}
+
+	private static double applyOperation(String op, double num2, double num1){
+		double result = 0;
+		switch (op) {
+				case "+":
+					result = num1 + num2;
+					break;
+				case "-":
+					result = num1 - num2;
+					break;
+				case "*":
+					result = num1 * num2;
+					break;
+				case "/":
+					result = num1 / num2;
+					break;
+				case "%":
+					result = num1 % num2;
+					break;
+		}
+		//System.out.println("result: " + result);
+		return result;
+	}
+
+	public static void main(String[] args){
+
+	}
+}
