93b335aab9933c5deff60468b147f7595fac6845
Michael Cheng <Michael Cheng>
2017-05-11 06:02:58 -0400

    Finish MyHeap.java

--- 13/MyHeap.java
+++ 13/MyHeap.java
@@ -1,52 +1,81 @@
 import java.util.*;
-public class MyHeap{
-    private ArrayList<String> heap;
-    private boolean isMax;
-   
-    public MyHeap(){
-	heap = new ArrayList<String>();
-	heap.add("null");
-	isMax = true;
-    }
-    
-    public MyHeap(boolean heapStatus){
-	isMax = heapStatus;
-    }
-    
-    private boolean pushUp(int index){
-	while(index > 1 && heap.get(index).compareTo(heap.get(index/2)) > 0){
-	    String temp = heap.get(index);
-	    heap.set(index, heap.get(index/2));
-	    heap.set(index/2, temp);
-	    index /= 2;
-	}
-	return true;
-    }
-    
-    private void pushDown(int index){
-    }
-    
-    public void add(String s){
-	heap.add(s);
-	pushUp(heap.size() - 1);
-    }
 
-    public String toString(){
-	String ans = "[";
-	for(String element : heap){
-	    ans += element + ", ";
-	    }
-	ans +=  "]";
-	return ans;
-    }
-    public static void main(String[] args){
-	MyHeap heap = new MyHeap();
-	heap.add("wow");
-	heap.add("a");
-	heap.add("c");
-	heap.add("d");
-	heap.add("b");
-	heap.add("zzz");
-	System.out.println(heap);
-    }
+public class MyHeap{
+	private ArrayList<String> heap;
+	private boolean isMax;
+	private int size;
+
+	public MyHeap(){
+		heap = new ArrayList<String>();
+		heap.add("null");
+		isMax = true;
+	}
+	
+	public MyHeap(boolean heapStatus){
+		heap = new ArrayList<String>();
+		isMax = heapStatus;
+		heap.add("null");
+	}
+	
+	private boolean pushUp(int index){
+		while(index > 1 && heap.get(index).compareTo(heap.get(index/2)) > 0 && isMax){
+			String temp = heap.get(index);
+			heap.set(index, heap.get(index/2));
+			heap.set(index/2, temp);
+			index /= 2;
+		}
+		while(index > 1 && heap.get(index).compareTo(heap.get(index/2)) < 0 && !isMax){
+			String temp = heap.get(index);
+			heap.set(index, heap.get(index/2));
+			heap.set(index/2, temp);
+			index /= 2;
+		}
+		return true;
+	}
+	
+	private boolean pushDown(int index){
+		while(index * 2 < heap.size - 1 && heap.get(index).compareTo(heap.get(index * 2) < 0 && isMax)){
+			String temp = heap.get(index * 2);
+			heap.set(index * 2, heap.get(index));
+			heap.set(index, temp);
+			index *= 2;
+		}
+		while(index * 2 < heap.size - 1 && heap.get(index).compareTo(heap.get(index * 2) > 0 && isMax)){
+			String temp = heap.get(index * 2);
+			heap.set(index * 2, heap.get(index));
+			heap.set(index, temp);
+			index *= 2;
+		}
+	}
+	
+	public void add(String s){
+		heap.add(s);
+		pushUp(heap.size() - 1);
+		size ++
+	}
+
+	public String remove(){
+		String temp = heap.get(1);
+		heap.set(1, heap.remove(heap.size() - 1));
+		pushDown(1);
+		return temp;
+	}
+
+	public String peek(){
+		return heap.get(1);
+	}
+
+	public String toString(){
+		return(Arrays.toString(heap.toArray()))
+	}
+	public static void main(String[] args){
+		MyHeap heap = new MyHeap();
+		heap.add("wow");
+		heap.add("a");
+		heap.add("c");
+		heap.add("d");
+		heap.add("b");
+		heap.add("zzz");
+		System.out.println(heap);
+	}
 }	
