d7f330842ad165c02b93cb9a7480a4e8387af56a
Michael Cheng <Michael Cheng>
2017-05-17 22:51:58 -0400

    Update /15Maze

--- 15Maze/FrontierStack.java
+++ 15Maze/FrontierStack.java
@@ -1,2 +1,18 @@
+import java.util.Stack;
 public class FrontierStack implements Frontier{
+	
+	private Stack<Location> locations;
+
+	public FrontierStack(){
+		locations = new Stack<Location>();
+	}
+
+	public void add(Location n){
+		locations.push(n);
+	}
+
+	public Location next(){
+		return locations.pop();
+	}
+
 }
--- 15Maze/MazeSolver.java
+++ 15Maze/MazeSolver.java
@@ -1,27 +1,40 @@
 public class MazeSolver{
-    private Maze board;
-    private boolean animate;
-    private String filename;
+	private Maze board;
+	private boolean animate;
+	private String filename;
+	private Frontier front;
 
-    public MazeSolver(String filename){
-	this(filename, false);
-    }
+	public MazeSolver(String filename){
+		this(filename, false);
+	}
 
-    public MazeSolver(String filename, boolean animate){
-	this.filename = filename;
-	this.animate = animate;
-    }
-    
-    public void solve(){
-	solve(1);
-    }
+	public MazeSolver(String filename, boolean animate){
+		this.filename = filename;
+		this.animate = animate;
+	}
 
-    public void solve(int style){
-    }
+	public void solve(){
+		solve(1);
+	}
 
-    public String toString(){
-	return board.toString();
-    }
+	public void solve(int style){
+		if(style = 0){
+            front = new FrontierStack();
+        }
+        if(style = 1){
+            //front = new FrontierQueue();
+        }
+        if(style = 2){
+            front = new FrontierPriorityQueue();
+        }
+        if(style = 3){
+            front = new FrontierStack();
+        }
+	}
+
+	public String toString(){
+		return board.toString();
+	}
 }
 
-  
+
